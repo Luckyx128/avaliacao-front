bf509d568e19b4182210808d8bc175b04f6ebf01
Lucas <lucas.de.amorimarq@gmail.com>
2025-02-27 10:45:19 -0300

    aaa

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/playground4',
     createProxyMiddleware({
-      target: 'http://172.32.0.102:8080',
+      target: 'http://172.32.1.73:5004',
       changeOrigin: true,
       pathRewrite: { '^/playground4': '' }, // Remove o prefixo
     })
