a18960a08a6136522e082907789764cf09793ead
Lucas <lucas.de.amorimarq@gmail.com>
2025-02-27 10:14:43 -0300

    aaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, href, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/dashboard';
 import Inicio from './components/inicio';
