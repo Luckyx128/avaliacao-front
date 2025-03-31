263b6d126e13e62d1629ff7232a0399e601a52e9
Lucas <lucas.de.amorimarq@gmail.com>
2025-03-31 12:14:26 -0300

    rota ajuste

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,10 @@
 // Função para obter a URL da API baseada no host atual
 const getApiUrl = () => {
   const currentHost = window.location.hostname;
-  if (currentHost === import.meta.env.VITE_HOST_REDE1 || currentHost === import.meta.env.VITE_HOST_REDE0 || currentHost === import.meta.env.VITE_HOST_REDE3) {
+  if (currentHost === import.meta.env.VITE_HOST_REDE4 ||
+      currentHost === import.meta.env.VITE_HOST_REDE1 ||
+      currentHost === import.meta.env.VITE_HOST_REDE0 ||
+      currentHost === import.meta.env.VITE_HOST_REDE3) {
     return import.meta.env.VITE_API_URL_REDE1;
   } else if (currentHost === import.meta.env.VITE_HOST_REDE2) {
     return import.meta.env.VITE_API_URL_REDE2;
